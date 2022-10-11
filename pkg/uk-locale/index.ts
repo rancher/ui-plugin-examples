1a52ed19d05331974f6ecede82c17e312bc76efc
Neil MacDougall <nmacdougall@suse.com>
2022-10-11 14:44:52 +0100

    Add UK locale

--- pkg/uk-locale/index.ts
+++ pkg/uk-locale/index.ts
@@ -1,18 +1,16 @@
 import { importTypes } from '@rancher/auto-import';
 import { IPlugin } from '@shell/core/types';
-import NewHomeComponent from './NewHomeComponent.vue';
 
 // Init the package
 export default function(plugin: IPlugin) {
+  console.error('ADD LOCALE !!!!!!!!!!!!!!1');
+
   // Auto-import model, detail, edit from the folders
   importTypes(plugin);
 
   // Provide plugin metadata from package.json
   plugin.metadata = require('./package.json');
 
-  plugin.addRoute({
-    name:      'home',
-    path:      '/home',
-    component: NewHomeComponent
-  });
+  // Add the new locale
+  plugin.addLocale('en-uk', 'UK English');
 }
