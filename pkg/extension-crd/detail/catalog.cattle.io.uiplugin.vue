297562df7cfa281a25e6bd9ed42b7c0562241a2a
Cody Jackson <cody.jackson@suse.com>
2024-09-06 23:34:45 -0700

    Adding `extension-crd` and `extensions-api-demo`
    Both now work with `yarn dev` and `yarn serve-pkgs`.
    
    `extensions-api-demo` needs more work. There appears to be some problems with navigating around the app and not all extension points around banners seem to work.

---
+++ pkg/extension-crd/detail/catalog.cattle.io.uiplugin.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  props: {
+    value: {
+      type:     Object,
+      required: true
+    }
+  }
+};
+</script>
+<template>
+  <div>
+    <h1>{{ value.name }}</h1>
+    <h3>{{ value.version }}</h3>
+  </div>
+</template>
+<style lang="scss" scoped>
+</style>
