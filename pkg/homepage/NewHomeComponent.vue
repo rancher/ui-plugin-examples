f51e46e1d3167da84176024a2368f33ac78b48ee
Cody Jackson <cody.jackson@suse.com>
2024-09-06 23:16:51 -0700

    Including home and large-extension extensions
    Including both now that `yarn dev` and `yarn serve-pkgs` now function.

---
+++ pkg/homepage/NewHomeComponent.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name:   'NweHomePage',
+  layout: 'plain'
+};
+</script>
+<template>
+  <div>
+    <h1>Welcome to Rancher</h1>
+    <img src="./assets/rancher-logo.svg" height="64" />
+    <h3>Home Page Extension 0.3.0</h3>
+    <img src="./assets/rancher-home-animated.svg" />
+  </div>
+</template>
+<style lang="scss" scoped>
+  h1, h3 {
+    margin: 20px 0;
+    text-align: center;
+  }
+
+  h3 {
+    color: #2553FF;
+    font-weight: bold;
+  }
+</style>
---
+++ pkg/homepage/index.ts
@@ -0,0 +1,18 @@
+import { importTypes } from '@rancher/auto-import';
+import { IPlugin } from '@shell/core/types';
+import NewHomeComponent from './NewHomeComponent.vue';
+
+// Init the package
+export default function (plugin: IPlugin) {
+  // Auto-import model, detail, edit from the folders
+  importTypes(plugin);
+
+  // Provide plugin metadata from package.json
+  plugin.metadata = require('./package.json');
+
+  plugin.addRoute({
+    name: 'home',
+    path: '/home',
+    component: NewHomeComponent
+  });
+}
