a1f38a956cdb944cbc40bbbd52607afbfda0af0a
Alexandre Alves <alexandre.alves@suse.com>
2024-11-05 13:26:30 +0000

    create extension version greater than 30mb

--- pkg/large-extension/NewFeature.vue
+++ pkg/large-extension/NewFeature.vue
@@ -4,8 +4,8 @@ export default { name: 'LargeExtension' };
 </script>
 <template>
   <div class="container-ext">
-    <h1>Large extension demo (> 20mb) - cache testing</h1>
-    <img src="./assets/img/susecon_20204.png" />
+    <h1>Large extension demo (> 30mb) - cache testing</h1>
+    <img src="./assets/img/susecon_20204.jpg" />
   </div>
 </template>
 
