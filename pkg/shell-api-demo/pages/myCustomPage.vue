88a57febd1499a2ed5fd48d436d31e7d3e78a3ce
Alexandre Alves <alexandre.alves@suse.com>
2026-01-02 10:19:25 +0000

    address pr comments

--- pkg/shell-api-demo/pages/myCustomPage.vue
+++ pkg/shell-api-demo/pages/myCustomPage.vue
@@ -2,9 +2,13 @@
 <script>
 import HelloWorldComponent from '../components/HelloWorld.vue';
 import { NotificationLevel } from '@shell/types/notifications';
+import RcButton from '@components/RcButton/RcButton.vue';
 
 export default {
   name: 'ShellApiDemoPage',
+  components: {
+    RcButton,
+  },
   mounted () {
     console.log('Shell API Demo Page mounted', this.$shell);
   },
@@ -46,39 +50,27 @@ export default {
   <div>
     <h2>Shell API Demo</h2>
     <p>This is a custom page to showcase the Shell API capabilities.</p>
-    <br />
-    <br />
-    <button @click="slideInTrigger">Test Slide-in API</button>
-    <a href="https://extensions.rancher.io/extensions/next/shell-api/interfaces/SlideInApi" target="_blank" rel="noopener noreferrer">Documentation Reference</a>
-    <br />
+    <RcButton primary class="mt-40 mb-10" @click="slideInTrigger">Test Slide-in API</RcButton>
+    <a class="mb-20" href="https://extensions.rancher.io/extensions/next/shell-api/interfaces/SlideInApi" target="_blank" rel="noopener noreferrer">Documentation Reference</a>
     <code>
       this.$shell.slideIn.open(HelloWorldComponent, {
         title: 'Hello from SlideIn panel!'
       });
     </code>
-    <br />
-    <br />
-    <button @click="modalTrigger">Test Modal API</button>
-    <a href="https://extensions.rancher.io/extensions/next/shell-api/interfaces/ModalApi" target="_blank" rel="noopener noreferrer">Documentation Reference</a>
-    <br />
+    <RcButton primary class="mt-40 mb-10" @click="modalTrigger">Test Modal API</RcButton>
+    <a class="mb-20" href="https://extensions.rancher.io/extensions/next/shell-api/interfaces/ModalApi" target="_blank" rel="noopener noreferrer">Documentation Reference</a>
     <code>
       this.$shell.modal.open(HelloWorldComponent, {
         props: { title: 'Hello Modal' }
       });
     </code>
-    <br />
-    <br />
-    <button @click="notificationTrigger">Test Notification API</button>
-    <a href="https://extensions.rancher.io/extensions/next/shell-api/interfaces/NotificationApi" target="_blank" rel="noopener noreferrer">Documentation Reference</a>
-    <br />
+    <RcButton primary class="mt-40 mb-10" @click="notificationTrigger">Test Notification API</RcButton>
+    <a class="mb-20" href="https://extensions.rancher.io/extensions/next/shell-api/interfaces/NotificationApi" target="_blank" rel="noopener noreferrer">Documentation Reference</a>
     <code>
       this.$shell.notification.send(NotificationLevel.Success, 'Some notification title', 'Hello world! Success!', {});
     </code>
-    <br />
-    <br />
-    <button @click="systemApiTrigger">Test System API</button>
-    <a href="https://extensions.rancher.io/extensions/next/shell-api/interfaces/SystemApi" target="_blank" rel="noopener noreferrer">Documentation Reference</a>
-    <br />
+    <RcButton primary class="mt-40 mb-10" @click="systemApiTrigger">Test System API</RcButton>
+    <a class="mb-20" href="https://extensions.rancher.io/extensions/next/shell-api/interfaces/SystemApi" target="_blank" rel="noopener noreferrer">Documentation Reference</a>
     <code>
       {
         gitCommit: this.$shell.system.gitCommit,
@@ -90,9 +82,7 @@ export default {
         uiVersion: this.$shell.system.uiVersion          
       }
     </code>
-    <br />
-    <p>System API Data:</p>
-    <br />
+    <p class="mt-40 mb-20">System API Data:</p>
     <code>
       {{ systemApiData }}
     </code>
@@ -100,7 +90,7 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-button {
+.btn {
   max-width: fit-content;
 }
 </style>
